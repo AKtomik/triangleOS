5b03556e43470f85098c6597b51b99369f20375b
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-09-19 17:44:58 +0200

    refactor tree

--- core/loadTemplate.js
+++ core/loadTemplate.js
@@ -1,5 +1,8 @@
+const templatesFile = "templates.html";
+
+
 async function loadTemplates() {
-  const res = await fetch("templates.html");
+  const res = await fetch(templatesFile);
   const text = await res.text();
   const frag = document.createRange().createContextualFragment(text);
   document.body.appendChild(frag); // injects templates into DOM (but hidden)
