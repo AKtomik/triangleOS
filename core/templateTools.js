6265ae4427321e798affc1c08590ff4ad05e151c
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-09-20 17:32:11 +0200

    can clone no-template Node with Template.spawnDataTemple(). for eg cloning desk.

--- core/templateTools.js
+++ core/templateTools.js
@@ -1,27 +1,39 @@
 const templatesFile = "edit/templates.html";
 
 async function loadTemplates() {
-  console.log(`loading templates at ${templatesFile}...`);
-  const res = await fetch(templatesFile);
-  const text = await res.text();
-  const frag = document.createRange().createContextualFragment(text);
-  document.body.appendChild(frag); // injects templates into DOM (but hidden)
-  console.log(`templates loaded!`);
+	console.log(`loading templates at ${templatesFile}...`);
+	const res = await fetch(templatesFile);
+	const text = await res.text();
+	const frag = document.createRange().createContextualFragment(text);
+	document.body.appendChild(frag); // injects templates into DOM (but hidden)
+	console.log(`templates loaded!`);
 }
 
 document.addEventListener("DOMContentLoaded", loadTemplates);
 
 
 class Template {
+
+	/**
+	 * Duplicate <template> content and spawn it.
+	 * @param {string} dataTemplateId The data-template's element's id.
+	 * @param {string|Node} parent The parent where to spawn.
+	 * @returns Nothing.
+	 */
 	static spawn(templateId, parent)
 	{
 		let originalTemplate = document.getElementById(templateId);
 		if (originalTemplate == undefined)
 		{
-			console.error("couldnt find template with id:", parent);
+			console.error("couldnt find something with id:", templateId);
 			return;
 		}
-		let cloneTemplate = originalTemplate.content.cloneNode(true);
+		if (originalTemplate.tagName != "TEMPLATE")
+		{
+			console.error("spawning something that is not a template:", originalTemplate, "\nuse Template.spawnDataTemplate to fake a template.");
+			return;
+		}
+		let clone = originalTemplate.content.cloneNode(true);
 		if (typeof parent === 'string')
 			parent = document.getElementById(parent);
 		if (!parent instanceof Node)
@@ -29,7 +41,39 @@ class Template {
 			console.error("given parent is not a Node nor a valid node id:", parent);
 			return;
 		}
-		parent.appendChild(cloneTemplate);
+		parent.appendChild(clone);
+	}
+	
+	/**
+	 * Duplicate a full Node like a template content. The node need to have the data-template atr.
+	 * @param {string} dataTemplateId The data-template's element's id.
+	 * @param {string|Node} parent The parent where to spawn.
+	 * @returns Nothing.
+	 */
+	static spawnDataTemplate(dataTemplateId, parent)
+	{
+		let originalTemplate = document.getElementById(dataTemplateId);
+		if (originalTemplate == undefined)
+		{
+			console.error("couldnt find something with id:", templateId);
+			return;
+		}
+		if (!originalTemplate.hasAttribute("data-template"))
+		{
+			console.error("add the atr [data-template] to allow cloning with Template.spawnDataTemplate():", originalTemplate);
+			return;
+		}
+		let clone = originalTemplate.cloneNode(true);
+		clone.removeAttribute("id"); // avoid duplicate ID
+		clone.removeAttribute("data-template"); // avoid confusing clones
+		if (typeof parent === 'string')
+			parent = document.getElementById(parent);
+		if (!parent instanceof Node)
+		{
+			console.error("given parent is not a Node nor a valid node id:", parent);
+			return;
+		}
+		parent.appendChild(clone);
 	}
 }
 
