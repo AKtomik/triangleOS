86dc23c49fbb2405fc7b8046fbbc22b55bc84db9
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-09-20 16:40:57 +0200

    desktop is tos-desk-top and taskbar is tos-desk-bar

--- core/desk.js
+++ core/desk.js
@@ -14,6 +14,23 @@ class Desk extends Window {
 	}
 }
 
+class DeskTop extends HTMLElement {
+  constructor() {
+    super();
+		this.classList.add("tos-desk-top");
+  }
+}
+
+class DeskBar extends HTMLElement {
+  constructor() {
+    super();
+		this.classList.add("tos-desk-bar");
+  }
+}
+
+
 customElements.define("tos-desk", Desk);
+customElements.define("tos-desk-top", DeskTop);
+customElements.define("tos-desk-bar", DeskBar);
 
 export { Desk };
