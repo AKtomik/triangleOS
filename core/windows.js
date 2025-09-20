0335d7b066c5d021e27d6c0e7729ac41fd67479b
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-09-20 11:53:22 +0200

    window inherit from HTMLElement and is now created by making a <tos-window> html element

--- core/windows.js
+++ core/windows.js
@@ -1,26 +1,31 @@
 console.debug("hi");
 
 
-class Window {
-	constructor(el, doInitChildWindows = true, doInitChildContainers = false) {
-		if (el.__windowInstance)
+class Window extends HTMLElement {
+
+  constructor() {
+    super();
+    this.__initialized = false; // avoid double init
+  }
+
+	connectedCallback() {
+		if (this.__initialized)
 		{
-			console.error("trying to construct already built window: ", el);
+			console.error("trying to connect already connected window: ", el);
 			return;
 		}
 
-		el.__windowInstance = true;
-		console.debug("constructing window: ", el);
-		this.el = el;
+		this.__initialized = true;
+		console.debug("connecting window: ", this);
 
 		// get content
-		const contentHTML = el.innerHTML;
-		el.innerHTML = "";
+		const contentHTML = this.innerHTML;
+		this.innerHTML = "";
 
 		// Create/header
 		const header = document.createElement("header");
 		header.className = "window-header";
-		header.innerHTML = `<span>${el.dataset.title || "Sans titre"}</span>`;
+		header.innerHTML = `<span>${this.dataset.title || "Sans titre"}</span>`;
 
 		// Create/header/close
 		const closeBtn = document.createElement("button");
@@ -36,8 +41,8 @@ class Window {
 		// Gather
 		this.header = header;
 		this.content = content;
-		el.appendChild(header);
-		el.appendChild(content);
+		this.appendChild(header);
+		this.appendChild(content);
 
 		// Close
 		closeBtn.addEventListener("click", (e) => {
@@ -51,25 +56,16 @@ class Window {
 			e.stopPropagation(); // not propagging to parents
 			isDragging = true;
 			document.body.style.userSelect = "none";
-			offsetX = e.clientX - this.el.offsetLeft;
-			offsetY = e.clientY - this.el.offsetTop;
-			this.el.style.zIndex = Window.zCounter++;
+			offsetX = e.clientX - this.offsetLeft;
+			offsetY = e.clientY - this.offsetTop;
+			this.style.zIndex = Window.zCounter++;
 		});
 		document.addEventListener("mousemove", (e) => {
 			if (isDragging) {
-				const parentRect = this.el.parentElement.getBoundingClientRect();
-				const elRect = this.header.getBoundingClientRect();
-				const clampPixelWidth = 50;
-				// slide
-				let newLeft = e.clientX - offsetX;
-				let newTop = e.clientY - offsetY;
-				// clamp
-				//newLeft = Math.max(0, Math.min(newLeft, parentRect.width - elRect.width));
-				newLeft = Math.max(clampPixelWidth - elRect.width, Math.min(newLeft, parentRect.width - clampPixelWidth));
-				newTop = Math.max(0, Math.min(newTop, parentRect.height - elRect.height));
-				// apply
-				this.el.style.left = (newLeft) + "px";
-				this.el.style.top = (newTop) + "px";
+				let pos = { left: e.clientX - offsetX, top: e.clientY - offsetY };
+				pos = this.clampPos(pos);
+				this.style.left = (pos.left) + "px";
+				this.style.top = (pos.top) + "px";
 			}
 		});
 		document.addEventListener("mouseup", () => {
@@ -77,71 +73,65 @@ class Window {
 			document.body.style.userSelect = "";
 		});
 
-		// Focus
-		el.addEventListener("mousedown", (e) => {
-			e.stopPropagation();
-			this.el.style.zIndex = Window.zCounter++;
-		});
-		this.el.style.zIndex = Window.zCounter++;
+		// Open
+		this.style.left = 0 + "px";
+		this.style.top = 0 + "px";
+		this.clampSelf();
 
-		// Init sub windows
-		if (doInitChildWindows)
-			this.initChildrenWindows(content);
-		if (doInitChildContainers)
-			this.initChildrenContainers(content);
+		// Focus
+		this.addEventListener("mousedown", (e) => {
+			e.stopPropagation();
+			this.style.zIndex = Window.zCounter++;
+		});
+		this.style.zIndex = Window.zCounter++;
 	}
 
 	// inits
-	initChildrenWindows(container) {
-		// init direct childrens of container
-		container.querySelectorAll(":scope > .window").forEach(childEl => {new Window(childEl);});
-	}
-	initChildrenContainers(container) {
-		// init any childrens in a .window-container that is inside container
-		container.querySelectorAll(":scope .window-container > .window").forEach(childEl => {
-			new Window(childEl);
-		});
-	}
 	static initRoot() {
-		console.log("loading cascade...")
-		// cascade init
-		document.querySelectorAll(".window-container > .window").forEach(el => new Window(el));
-		// warn for not inited
-		document.querySelectorAll(".window").forEach(el => {
-			if (!el.__windowInstance) {
-				console.error("a window not loaded because is not a child of .window-container nor .window:\n", el);
-			}
-		});
-		// global init
 		Window.zCounter = 10;
-		console.log("cascade done!")
 	}
-
 
 	// closing
 	close() {
-		console.debug("destructing window: ", this.el);
-		this.el.remove();
+		console.debug("closing window: ", this);
+		this.remove();
+	}
+  //disconnectedCallback() {
+  //  console.debug("destructing window: ", this);
+  //}
+
+	// positioning
+	clampPos(pos) {
+		const parentRect = this.parentElement.getBoundingClientRect();
+		const elRect = this.header.getBoundingClientRect();
+		const clampPixelWidth = 50;
+		const newTop = Math.max(0, Math.min(pos.top, parentRect.height - elRect.height));
+		//newLeft = Math.max(0, Math.min(pos.left, parentRect.width - elRect.width));
+		const newLeft = Math.max(clampPixelWidth - elRect.width, Math.min(pos.left, parentRect.width - clampPixelWidth));
+		return { top: newTop, left: newLeft };
+	}
+	clampSelf() {
+		let pos = { left: this.offsetLeft, top: this.offsetTop };
+		pos = this.clampPos(pos);
+		this.style.left = (pos.left) + "px";
+		this.style.top = (pos.top) + "px";
 	}
 
 	// static
 	static __makeBaseWindow(windowTitle, windowClass) {
-		const element = document.createElement("div");
+		const element = document.createElement("tos-window");
 		element.dataset.title = windowTitle;
 		element.className = windowClass;
-		element.classList.add("window");
 		return element;
 	}
 	static __makeChild(parent, element) {
 		if (typeof parent === 'string')
 			parent = document.getElementById(parent);
-		console.log("parent=",parent)
 		parent.appendChild(element);
 	}
 	static createHtmlWindow(parent, windowTitle, windowClass, htmlSrc) {
 		const element = Window.__makeBaseWindow(windowTitle, windowClass);
 		element.innerHTML = `<object type="text/html" data="${htmlSrc}"></object>`;
-		new Window(element, true, true);
 		Window.__makeChild(parent, element);
 	}
 	static createTemplateWindow(parent, windowTitle, windowClass, templateId) {
@@ -149,18 +139,17 @@ class Window {
 		let template = document.getElementById(templateId);
 		let nodeTemplate = template.content.cloneNode(true);
 		element.appendChild(nodeTemplate);
-		new Window(element, true, true);
 		Window.__makeChild(parent, element);
 	}
 	static createIframeWindow(parent, windowTitle, windowClass, iframeUrl) {
 		const element = Window.__makeBaseWindow(windowTitle, windowClass);
 		element.innerHTML = `<iframe src="${iframeUrl}"></iframe>`;
-		new Window(element, true, true);
 		Window.__makeChild(parent, element);
 	}
 }
 
 // Lisener
 document.addEventListener("DOMContentLoaded", Window.initRoot);
+customElements.define("tos-window", Window);
 
 export { Window };
