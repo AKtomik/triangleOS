305711eec377f1a47902f8f544fe146cfc5ed80b
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-09-20 17:52:18 +0200

    clarification on spawnDataTemplate.

--- core/templateTools.js
+++ core/templateTools.js
@@ -33,7 +33,6 @@ class Template {
 			console.error("spawning something that is not a template:", originalTemplate, "\nuse Template.spawnDataTemplate to fake a template.");
 			return;
 		}
-		let clone = originalTemplate.content.cloneNode(true);
 		if (typeof parent === 'string')
 			parent = document.getElementById(parent);
 		if (!parent instanceof Node)
@@ -41,11 +40,13 @@ class Template {
 			console.error("given parent is not a Node nor a valid node id:", parent);
 			return;
 		}
+		let clone = originalTemplate.content.cloneNode(true);
 		parent.appendChild(clone);
 	}
 	
 	/**
-	 * Duplicate a full Node like a template content. The node need to have the data-template atr.
+	 * Duplicate Node's childs like it is a template. The node need to have the data-template atr.
+	 * WARNING: cloning inited window wont work.
 	 * @param {string} dataTemplateId The data-template's element's id.
 	 * @param {string|Node} parent The parent where to spawn.
 	 * @returns Nothing.
@@ -63,9 +64,6 @@ class Template {
 			console.error("add the atr [data-template] to allow cloning with Template.spawnDataTemplate():", originalTemplate);
 			return;
 		}
-		let clone = originalTemplate.cloneNode(true);
-		clone.removeAttribute("id"); // avoid duplicate ID
-		clone.removeAttribute("data-template"); // avoid confusing clones
 		if (typeof parent === 'string')
 			parent = document.getElementById(parent);
 		if (!parent instanceof Node)
@@ -73,7 +71,14 @@ class Template {
 			console.error("given parent is not a Node nor a valid node id:", parent);
 			return;
 		}
-		parent.appendChild(clone);
+		// cloning parent
+		let clone = originalTemplate.cloneNode(true);
+		clone.removeAttribute("id"); // avoid duplicate ID
+		clone.removeAttribute("data-template"); // avoid confusing clones
+		// or just its childs
+		//let html = originalTemplate.innerHTML;
+		//console.log("originalTemplate.innerHTML=",originalTemplate.innerHTML);
+		//parent.insertAdjacentHTML("beforeend", html);
 	}
 }
 
