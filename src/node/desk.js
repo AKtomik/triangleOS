3fbb40615b566581db0418d673fd5db8072ca77f
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-09-22 03:06:26 +0200

    desktop icon basic logic

--- src/node/desk.js
+++ src/node/desk.js
@@ -1,17 +1,13 @@
 import { Window } from "/src/node/windows.js";
 
-class Desk extends Window {
+// STRUCTURE
 
+class Desk extends Window {
   constructor() {
     super();
 		this.classList.add("tos-desk");
 		console.log("construct Desk");
   }
-
-	connectedCallback() {
-		super.connectedCallback?.();
-		console.log("connect Desk");
-	}
 }
 
 class DeskTop extends HTMLElement {
@@ -28,9 +24,45 @@ class DeskBar extends HTMLElement {
   }
 }
 
-
 customElements.define("tos-desk", Desk);
 customElements.define("tos-desktop", DeskTop);
 customElements.define("tos-deskbar", DeskBar);
+
+// DESKTOP
+
+class DesktopIcon extends HTMLElement {
+  constructor() {
+    super();
+		this.classList.add("tos-desktop-icon");
+  }
+
+  connectedCallback() {
+    this.name = this.dataset.name;
+
+    let mouseSelect = (e) => {
+      document.querySelectorAll('.tos-desktop-icon.selected').forEach(i => i.classList.remove('selected'));
+      this.classList.add('selected');
+    };
+		this.onpointerdown = mouseSelect;
+  }
+
+	get name()
+	{
+		return this.querySelector("span").textContent;
+	}
+	set name(value)
+	{
+		let span = this.querySelector("span");
+		if (!span)
+		{
+			span = document.createElement("span");
+			this.appendChild(span);
+		}
+		span.textContent = value;
+	}
+}
+
+customElements.define("tos-desktop-icon", DesktopIcon);
+
 
 export { Desk };
