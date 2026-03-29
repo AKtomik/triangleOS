9733bd5fd1df2e7d1e80721b95f7d77c35687221
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-03-29 21:11:31 +0200

    catalog registerSize and static skinCollection

--- src/vanilla/catalog.js
+++ src/vanilla/catalog.js
@@ -1,15 +1,24 @@
 
 
 class Catalog {
-	static sizeList = ['size-full', 'size-small', 'size-middle'];
+	static sizeList = [];
 
-	static skinListType = {
+	static registerSize(...sizeClassNameList)
+	{
+		sizeClassNameList.forEach(sizeClassName => {
+			this.sizeList.push(sizeClassName);
+		});
+	}
+
+	static skinCollection = {
 		content: ['skin-white', 'skin-lightgray', 'skin-gray', 'skin-black', 'skin-brown', 'skin-red', 'skin-orange', 'skin-yellow', 'skin-lime', 'skin-green', 'skin-cyan', 'skin-lightblue', 'skin-blue', 'skin-purple', 'skin-magenta', 'skin-pink', 'skin-transparent', 'skin-glass', 'skin-filter-negative', 'skin-filter-grayscale', 'skin-filter-saturate', 'skin-deep'],
 		top: ['skin-top-white', 'skin-top-lightgray', 'skin-top-gray', 'skin-top-black', 'skin-top-brown', 'skin-top-red', 'skin-top-orange', 'skin-top-yellow', 'skin-top-lime', 'skin-top-green', 'skin-top-cyan', 'skin-top-lightblue', 'skin-top-blue', 'skin-top-purple', 'skin-top-magenta', 'skin-top-pink', 'skin-top-transparent', 'skin-top-glass', 'skin-top-filter-negative', 'skin-top-filter-grayscale', 'skin-top-filter-saturate', 'skin-top-deep'],
 		all: [],
 	}
 
-	static skinList = [...this.skinListType.content, ...this.skinListType.top, ...this.skinListType.all];
+	static skinList = [...this.skinCollection.content, ...this.skinCollection.top, ...this.skinCollection.all];
 }
+
+Catalog.registerSize('size-full', 'size-small', 'size-middle');
 
 export { Catalog };
