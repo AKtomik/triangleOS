c80780c857feaae48e5c90abe8b2ecd83482634b
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-03-29 21:55:30 +0200

    skin affecting content are in the 'in' collection

--- src/vanilla/catalog.js
+++ src/vanilla/catalog.js
@@ -72,7 +72,7 @@ class Catalog {
 }
 
 Catalog.registerSize(['size-full', 'size-small', 'size-middle']);
-Catalog.registerSkin(['skin-white', 'skin-lightgray', 'skin-gray', 'skin-black', 'skin-brown', 'skin-red', 'skin-orange', 'skin-yellow', 'skin-lime', 'skin-green', 'skin-cyan', 'skin-lightblue', 'skin-blue', 'skin-purple', 'skin-magenta', 'skin-pink', 'skin-transparent', 'skin-glass', 'skin-filter-negative', 'skin-filter-grayscale', 'skin-filter-saturate', 'skin-deep']);
+Catalog.registerCollection('in', ['skin-in-white', 'skin-in-lightgray', 'skin-in-gray', 'skin-in-black', 'skin-in-brown', 'skin-in-red', 'skin-in-orange', 'skin-in-yellow', 'skin-in-lime', 'skin-in-green', 'skin-in-cyan', 'skin-in-lightblue', 'skin-in-blue', 'skin-in-purple', 'skin-in-magenta', 'skin-in-pink', 'skin-in-transparent', 'skin-in-glass', 'skin-in-filter-negative', 'skin-in-filter-grayscale', 'skin-in-filter-saturate', 'skin-in-deep']);
 Catalog.registerCollection('top', ['skin-top-white', 'skin-top-lightgray', 'skin-top-gray', 'skin-top-black', 'skin-top-brown', 'skin-top-red', 'skin-top-orange', 'skin-top-yellow', 'skin-top-lime', 'skin-top-green', 'skin-top-cyan', 'skin-top-lightblue', 'skin-top-blue', 'skin-top-purple', 'skin-top-magenta', 'skin-top-pink', 'skin-top-transparent', 'skin-top-glass', 'skin-top-filter-negative', 'skin-top-filter-grayscale', 'skin-top-filter-saturate', 'skin-top-deep']);
 
 export { Catalog };
